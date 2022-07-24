d7d863824456dc84160283c0ab1db53b8ca8bd9b
ashura-code <83760410+ashura-code@users.noreply.github.com>
2022-07-24 16:32:44 +0530

    cheap css done

--- src/Move.jsx
+++ src/Move.jsx
@@ -2,7 +2,7 @@ const move = ({card}) => {
     return (
         <div className='movie'>
     
-        <img src={(card.Poster==="N/A" && 'https://via.placeholder.com/400') || card.Poster} alt={card.Title} title={card.Title}></img>
+        <img src={(card.Poster==="N/A" && 'https://via.placeholder.com/300x413') || card.Poster} alt={card.Title} title={card.Title}></img>
         <h4>{card.Title}</h4>
         </div>
      );
